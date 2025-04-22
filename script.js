4f22a1e8c04a9b1416a1ee61bb01e5ec9815cf0c
fufex3934 <fufawakgari174@gmail.com>
2025-04-22 16:37:12 +0300

    dark and light mode

---
+++ script.js
@@ -0,0 +1,27 @@
+let html = document.getElementById("html");
+let change_btn = document.getElementById("change_btn");
+let change_img = document.getElementById("change_img");
+
+change_btn.addEventListener("click",function(){
+  html.classList.toggle("dark");
+  if(change_img.src.includes('img/moon.svg')){
+    change_img.src = "img/sun.svg"
+
+  }else{
+    change_img.src = "img/moon.svg"
+  }
+});
+
+
+let change_btn2 = document.getElementById("change_btn2");
+let change_img2 = document.getElementById("change_img2");
+
+change_btn2.addEventListener("click",function(){
+  html.classList.toggle("dark");
+  if(change_img2.src.includes('img/moon.svg')){
+    change_img2.src = "img/sun.svg"
+
+  }else{
+    change_img2.src = "img/moon.svg"
+  }
+})
